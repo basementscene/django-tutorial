0a40a2a93549bd7ca1bd632c87974bfc335ff378
Valeria <lera.romanciuc@gmail.com>
2020-04-08 21:24:10 +0100

    setup of admin dashboard and superuser created

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -8,3 +8,6 @@ def index(response, name):
     ls = ToDoList.objects.get(name=name)
     item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br><p>%s</p>" % (ls.name, str(item.text)))
+
+def home(response):
+    return HttpResponse("<h1>Home</h1>")
