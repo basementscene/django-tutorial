51df91680c2f533369a8f3efab701a3393514a63
Valeria <lera.romanciuc@gmail.com>
2020-04-09 13:39:21 +0100

    added css

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -8,6 +8,6 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("create/", views.create, name="create")
 ]
